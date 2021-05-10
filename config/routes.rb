95e8eea6dce81f43dcfbbad5b533827a0288f207
Stephane Lafontaine <stephane@medqigong.ca>
2021-05-10 10:52:45 -0400

    Lecture version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # verb 'path', to: 'controller#action'
+  # A special route
   root to: 'pages#home'
 
+  # verb 'path', to: 'controller#action'
   get '/about', to: 'pages#about', as: :about_us
   get '/contact', to: 'pages#contact'
 end
